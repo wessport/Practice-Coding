04988633dec4a1c17127ab2b6d8b90f524aa240e
wessport <wesporter92@gmail.com>
2016-11-14 14:42:45 -0600

    A first commit setting up git

---
+++ Practice-Coding/Scratch Paper Python.py
@@ -0,0 +1,19 @@
+# Scratch Paper used for learning purposes
+
+# To run this code, open the Command Prompt/Shell and type the following:
+# python.exe "C:\Users\Wesley\wessport\github\Scratch Paper Python.py"
+
+
+ingredients = ['snails', 'leeches', 'gorilla belly-button lint',
+               'caterpillar eyebrows', 'centipede toes']
+
+Earth_weight = 130
+Moon_weight = Earth_weight * 0.165
+year = 1
+j = 0
+
+for i in range(0, 14):
+    Moon_weight = Moon_weight + j
+    print(('%s %s') % (year, Moon_weight))
+    year = year + 1
+    j = Moon_weight
