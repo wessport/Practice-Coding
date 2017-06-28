926b297bca22f4c95a11a0e90657abbad636ed58
Wes Porter <wesporter92@gmail.com>
2017-06-28 14:35:46 -0500

    Added plot labels and color

--- R_Scripts/Plotting_Erosion.R
+++ R_Scripts/Plotting_Erosion.R
@@ -7,17 +7,37 @@
 
 library(ggplot2)
 library(readr)
+library(ggthemes)
 
+# Read in the cell data
 cell83 <- read_csv("E:/Wes/Work/Rusle2/R_input/cell83.csv")
 
-theme_update(plot.title = element_text(hjust = 0.5))
+# Convert Scenario# into factor for color assignment purposes
+cell83$Scenario <- as.factor(cell83$Scenario)
+Scenario <- cell83$Scenario
 
-g <- ggplot(cell83, aes(Year_frac, Erosion)) + geom_point()
+# Specify the specific font family to be used in the plot
+windowsFonts(Times=windowsFont("TT Arial"))
+
+# Plot the cell data using ggplot2
+g <- ggplot(cell83, aes(x = Year_frac, y = Erosion, color = Scenario, group = Scenario))
+g <- g + geom_point(aes(shape=Scenario)) + geom_line() + theme_few()
 g <- g + ggtitle('Cell 83')
+g <- g + labs(x = "Year", y = "Erosion [t/ac]")
 g <- g + theme(plot.title = element_text(
   size = 20,
   face = "bold",
-  margin = margin(10, 0, 10, 0),
-  element_text(hjust = 0.5)
-))
+  margin = margin(10, 0, 10, 0), # Adds vertica whitespace around plot title 
+  hjust = 0.5, # centers the title
+  family = "Arial"# Font family
+)) 
+g <- g + scale_x_continuous(
+  limits = c(1982, 1994),
+  breaks = c(1982, 1984, 1986, 1988, 1990, 1992, 1994)
+)
+
 g
+
+
+filename <- "Cell 83"
+ggsave(filename, plot = last_plot(), device = "png", width = 5, height = 4, units = "in", dpi= 300)
